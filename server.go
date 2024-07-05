1065bc8b087dc7c6a200ca55e1b27d3a45590ecf
Nazario A. Ayala <nazario.ayala@transcarent.com>
2024-07-05 16:30:45 -0700

    Adde call to make connection to sql

--- server.go
+++ server.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"deep-link-api/internal"
 	"github.com/labstack/echo/v4"
 	"net/http"
 )
 
 func main() {
+	// TODO: Get env variable and pass to InitDb
+	err := internal.InitDb("")
+
+	// TODO: defer close on panic?
+
+	// This is a panic because the server should not start if the database is not connected
+	if err != nil {
+		panic(err)
+	}
+
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
